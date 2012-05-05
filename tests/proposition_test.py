c8ab62f2b5d022e53e3489e1ca64171b5d36fff3
Niko W <der@nik0.de>
2012-05-05 20:31:32 +0200

    Something like a first test.

---
+++ tests/proposition_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from proposition import Proposition
+
+class PropositionTest(unittest.TestCase):
+	def setUp(self):
+		self.prop = Proposition('foo', 20120430, 'http://www.frankfurt-gestalten.de', 'APPD', 'OF_1234', 1234)
+
+	def tearDown(self):
+		del self.prop
+
+	def testTitle(self):
+		self.assertEqual('foo', self.prop.title)
+
+	def testURL(self):
+		self.assertEqual('http://www.frankfurt-gestalten.de', self.prop.link)
+
+	def testParty(self):
+		self.assertEqual('APPD', self.prop.party)
