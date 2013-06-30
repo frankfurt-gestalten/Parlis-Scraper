8ac55383161b0de9f9f3a35facd904704f47c03e
Niko Wenselowski <der@nik0.de>
2013-06-30 20:41:56 +0200

    Autopep8.

--- exporters/CSVExporter.py
+++ exporters/CSVExporter.py
@@ -2,41 +2,44 @@ from csv import writer
 
 from exporters import Exporter
 
+
 class CSVExporter(Exporter):
     '''
-    A class to export the propositions into a .csv-file. 
+    A class to export the propositions into a .csv-file.
     '''
-    
+
     def __init__(self, filename):
         self.outputFile = filename
-        
+
     def createExport(self, propositions):
         """
         This will append the proposition to the export file.
         """
         csvWriter = writer(
-                           open(self.outputFile, 'ab'), 
-                           delimiter=';', 
-                           quotechar='"'
-                           )
-        header = ["update","link","date","partei","nummer","title","begruendung","betreff","ergebnisse", 'ob_nummer']
+            open(self.outputFile, 'ab'),
+            delimiter=';',
+            quotechar='"'
+        )
+        header = ["update", "link", "date", "partei", "nummer",
+                  "title", "begruendung", "betreff", "ergebnisse", 'ob_nummer']
         csvWriter.writerow(header)
         i = 1
         for singleProposition in propositions:
             if(singleProposition is not None and i <= 500):
-                csvWriter.writerow(self._createSequenceFromProposition(singleProposition))
-                i +=1
+                csvWriter.writerow(
+                    self._createSequenceFromProposition(singleProposition))
+                i += 1
 
     def _createSequenceFromProposition(self, proposition):
         return [
-                proposition.updateDate,
-                proposition.link,
-                proposition.date,
-                proposition.party,
-                proposition.proposition_nbr,
-                proposition.title,
-                proposition.statement,
-                proposition.subject,
-                proposition.result,
-                proposition.obnumber
-                ]
+            proposition.updateDate,
+            proposition.link,
+            proposition.date,
+            proposition.party,
+            proposition.proposition_nbr,
+            proposition.title,
+            proposition.statement,
+            proposition.subject,
+            proposition.result,
+            proposition.obnumber
+        ]
