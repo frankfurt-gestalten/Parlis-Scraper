ee37ba0a05a1b72a6f4d4a46807c560f0ec1eea1
Niko Wenselowski <der@nik0.de>
2015-02-22 13:08:28 +0100

    Converting to spaces.

--- tests/proposition_test.py
+++ tests/proposition_test.py
@@ -6,24 +6,31 @@ from parlisscraper.proposition import Proposition
 
 
 class PropositionTest(unittest.TestCase):
-	def setUp(self):
-		self.prop = Proposition('foo', 20120430, 'http://www.frankfurt-gestalten.de', 'APPD', 'OF_1234', 1234)
+    def setUp(self):
+        self.prop = Proposition(
+            'foo',
+            20120430,
+            'http://www.frankfurt-gestalten.de',
+            'APPD',
+            'OF_1234',
+            1234
+        )
 
-	def tearDown(self):
-		del self.prop
+    def tearDown(self):
+        del self.prop
 
-	def testTitle(self):
-		self.assertEqual('foo', self.prop.title)
+    def testTitle(self):
+        self.assertEqual('foo', self.prop.title)
 
-	def testURL(self):
-		self.assertEqual('http://www.frankfurt-gestalten.de', self.prop.link)
+    def testURL(self):
+        self.assertEqual('http://www.frankfurt-gestalten.de', self.prop.link)
 
-	def testParty(self):
-		self.assertEqual('APPD', self.prop.party)
+    def testParty(self):
+        self.assertEqual('APPD', self.prop.party)
 
-	def test__repr__(self):
-		representation = repr(self.prop)
+    def test__repr__(self):
+        representation = repr(self.prop)
 
-		self.assertTrue(self.prop.title in representation)
-		self.assertTrue(self.prop.link in representation)
-		self.assertTrue(self.prop.party in representation)
+        self.assertTrue(self.prop.title in representation)
+        self.assertTrue(self.prop.link in representation)
+        self.assertTrue(self.prop.party in representation)
